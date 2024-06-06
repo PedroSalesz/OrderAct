8b22b16cd5cd151daef949042ea2daf249118795
Pedro Sales <pedrosalesdev@gmail.com>
2024-06-06 06:49:56 -0300

    Adicionando comentários dentro do codigo para partes importantes do programa

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -10,7 +10,10 @@ namespace ProjectOrder.Entities
 {
     internal class Order
     {
-        public DateTime Moment { get; set; }
+        //Dentro do "Order(pedido) vou ter os atributos do que se tem em um pedido, logo terei os atributos que compõe o pedido
+        //Momento, status do pedido, o cliente que vai fazer o pedido e uma lista de Items. Sendo assim todos são ligados ao pedido
+        
+        public DateTime Moment { get; set; }   
         public OrderStatus Status { get; set; }
         public Client Client { get; set; }
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
@@ -19,7 +22,7 @@ namespace ProjectOrder.Entities
         {
         }
 
-        public Order(DateTime moment, OrderStatus status, Client client)
+        public Order(DateTime moment, OrderStatus status, Client client)  //Construtor que pede de paramentro o momento do pedido, status e o cliente
         {
             Moment = moment;
             Status = status;
--- Program.cs
+++ Program.cs
@@ -13,28 +13,28 @@ Console.WriteLine("Enter order data: ");
 Console.Write("Status: ");
 OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
 
-Client client = new Client(clientName, email, birthDate);
-Order order = new Order(DateTime.Now, status, client);
+Client client = new Client(clientName, email, birthDate);  //Construtor recebendo nome do cliente, email do cliente e data de aniversário
+Order order = new Order(DateTime.Now, status, client);    // Construtor recebendo a data do momento, o status do pedido e o cliente
 
 Console.Write("How many items to this order? ");
 int n = int.Parse(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine($"Enter #{i} item data:");
+    Console.WriteLine($"Enter #{i} item data:"); //Interpolação ($"Enter #{i} item data:");
     Console.Write("Product name: ");
     string productName = Console.ReadLine();
     Console.Write("Product price: ");
     double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-    Product product = new Product(productName, price);
+    Product product = new Product(productName, price); //Construtor recebendo Nome do produto e o preço
 
     Console.Write("Quantity: ");
     int quantity = int.Parse(Console.ReadLine());
 
-    OrderItem orderItem = new OrderItem(quantity, price, product);
+    OrderItem orderItem = new OrderItem(quantity, price, product); //Construtor recebendo a Quantidade, preço e o produto
 
-    order.AddItem(orderItem);
+    order.AddItem(orderItem); //Adicionando items 
 }
 
 Console.WriteLine();
